2ae6ef43c7176ae096df3f83a4a89a56eb5c95e6
MrBlackIsBack <jamesbehan198@gmail.com>
2015-01-12 20:13:50 +0000

    Initial commit

---
+++ src/me/mrblackisback/mobspawn/MobSpawn.java
@@ -0,0 +1,14 @@
+package me.mrblackisback.mobspawn;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MobSpawn extends JavaPlugin {
+
+    public void onEnable() {
+
+    }
+
+    public void onDisable() {
+
+    }
+}
